7e3dc910199fdb81b1833f8184208ed9f182152d
katya1nik <katya77nik@gmail.com>
2025-01-06 23:53:00 +0300

    Создала метод для записи в файл

--- hw_lesson_16_oop_1.py
+++ hw_lesson_16_oop_1.py
@@ -1,4 +1,3 @@
-
 class TxtFileHandler:
     """Создала класс для работы с файлом"""
     def read_file(self, filepath: str) -> str:
@@ -11,7 +10,21 @@ class TxtFileHandler:
         except Exception as e:
             print(f"Ошибка при чтении файла: {e}")
             return ""
-handler =  TxtFileHandler()    
+    
 # Добавила исключения для обработки ошибок при чтении файла.
+
+
+    def write_file(self,filepath: str, *data: str) -> None:
+        """Метод для записи данных в файл"""
+        try:
+            with open(filepath, 'w', encoding='utf-8') as file:
+                for line in data:
+                    file.write(line + '\n')
+        except Exception as e:
+            print(f"Ошибка при записи в файл: {e}")
+
+handler =  TxtFileHandler()
+
 content = handler.read_file("test.txt")
 print(content)
+handler.write_file("test.txt", "This is a test string.\n")
